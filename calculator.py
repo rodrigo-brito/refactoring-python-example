b2e381ba355360197f99fbeed1714f9f5702f504
Rodrigo Brito <rig.10@hotmail.com>
2021-04-02 13:28:52 -0300

    initial commit

---
+++ calculator.py
@@ -0,0 +1,13 @@
+
+class Calculator:
+	def sum(self, a, b):
+		return a + b;
+
+	def mult(self, a, b):
+		return a * b;
+
+	def printIsEven(self, value):
+		if value % 2 == 0:
+			print("even")
+		else:
+			print("odd")
---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from calculator import Calculator 
+
+calculator = Calculator()
+print("1 + 2 = ", calculator.sum(1, 2))
+print("2 x 2 = ", calculator.mult(2, 2))
+print("2 is = ",)
+calculator.printIsEven(2)
+print("3 is = ",)
+calculator.printIsEven(3)
